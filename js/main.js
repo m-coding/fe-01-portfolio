b47f51e20710252e2c396f9d96cc9f33138c3818
M <m-coding@users.noreply.github.com>
2015-12-31 01:48:31 -0800

    First commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// Slide show
+$('#camera_wrap_1').camera({
+	cols: 12,
+	rows: 7,
+	time: 5000,
+	thumbnails: false,
+	fx: 'mosaicRandom',
+	pagination: false
+});
+
+// Show modal dialog when an image thumbnail is clicked
+$('.enableDialog').click(function(event){
+    var $img      = $(this);
+    var $imgClone = $img.clone().removeClass('highlight');
+    var $title    = $img.attr('alt');
+    var $text     = $('#' + $img.data('city')).text();
+
+    new BootstrapDialog({
+		closable : true,
+		title    : $title,
+		message  : function(dialog) {
+			var $msg = $('<div></div>');
+			$msg.append($imgClone);
+			$msg.append('<p class="msg">' + $text + '</p>');
+
+			return $msg;
+		}
+    }).open();
+
+    event.preventDefault();
+});
