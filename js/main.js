3947b2bd4025da21786a5e988fbb8c7e453f210a
M <m-coding@users.noreply.github.com>
2015-12-31 02:02:19 -0800

    Fix tabbing

--- js/main.js
+++ js/main.js
@@ -10,22 +10,22 @@ $('#camera_wrap_1').camera({
 
 // Show modal dialog when an image thumbnail is clicked
 $('.enableDialog').click(function(event){
-    var $img      = $(this);
-    var $imgClone = $img.clone().removeClass('highlight');
-    var $title    = $img.attr('alt');
-    var $text     = $('#' + $img.data('city')).text();
+	var $img      = $(this);
+	var $imgClone = $img.clone().removeClass('highlight');
+	var $title    = $img.attr('alt');
+	var $text     = $('#' + $img.data('city')).text();
 
-    new BootstrapDialog({
-		closable : true,
-		title    : $title,
-		message  : function(dialog) {
-			var $msg = $('<div></div>');
-			$msg.append($imgClone);
-			$msg.append('<p class="msg">' + $text + '</p>');
+	new BootstrapDialog({
+	closable : true,
+	title    : $title,
+	message  : function(dialog) {
+		var $msg = $('<div></div>');
+		$msg.append($imgClone);
+		$msg.append('<p class="msg">' + $text + '</p>');
 
-			return $msg;
-		}
-    }).open();
+		return $msg;
+	}
+	}).open();
 
-    event.preventDefault();
+	event.preventDefault();
 });
